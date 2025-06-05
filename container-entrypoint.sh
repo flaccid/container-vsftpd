065ace6d0a0f7aff172fc79454e5fbeb0a974d6f
Chris Fordham <chris@fordham.id.au>
2025-06-05 20:06:20 +1000

    Change location of vsftpd-passwd.

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -e
 
 # automatic user provisioning
-if [ ! -z /tmp/vsftpd-passwd ]; then
-	echo 'provisioning users from /tmp/vsftpd-passwd'
+if [ ! -e /mnt/vsftpd/vsftpd-passwd ] && [ ! -z /mnt/vsftpd/vsftpd-passwd ]; then
+	echo 'provisioning users from /mnt/vsftpd/vsftpd-passwd'
     while IFS= read -r line; do
         user=${line%:*}
         echo "adding system user, ${user}"
         adduser -D "$user"
         echo $line | chpasswd
-    done < /tmp/vsftpd-passwd
+    done < /mnt/vsftpd/vsftpd-passwd
     # empty the file after use (if possible) for security reasons
-    (> /tmp/vsftpd-passwd || true) > /dev/null 2>&1
+    (> /mnt/vsftpd/vsftpd-passwd || true) > /dev/null 2>&1
 fi
 
 tail -f /var/log/vsftpd.log &
